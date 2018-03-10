2c83b54519bc6576931dbb612f894d1050f65738
egva <fearfelix@yandex.com>
2018-03-10 15:04:33 +0300

    Bölüm 1 Route Yapısı

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -14,3 +14,25 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+#Bu komut ile tarayıcıya / dan sonra merhaba yazdığımızda istediğimiz metni yazdırabiliyoruz.
+
+Route::get('/merhaba' , function (){
+
+   return "Merhaba!!!";
+});
+
+#Json formatında veri gönderme işlemi
+
+Route::get('/api/v3/' , function (){
+
+    return ['mesaj' => 'Merhaba'];
+});
+
+
+#Parametre ile route kullanımı
+
+Route::get('/urun/{urunadi}/{id}',function ($urunadi, $id){
+
+    return "Ürün Adı: $id $urunadi";
+});
