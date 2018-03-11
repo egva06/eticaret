c8c59675636b1ed219c2e59ca3bce3262bbba8c3
egva <fearfelix@yandex.com>
2018-03-11 23:52:54 +0300

    Laravel Eticaret sass falan yüklendi

---
+++ eticaret/resources/views/layouts/master.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel Eticaret</title>
+
+    @include('layouts.partials.head')
+    @yield('head')
+</head>
+
+<body id="commerce">
+
+@include('layouts.partials.navbar')
+
+@yield('content')
+
+@include('layouts.partials.footer')
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+@yield('footer')
+</body>
+
+</html>
