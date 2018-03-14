f019f2c3788ab5d45a6812ae65c65f67165c8ba5
egva <fearfelix@yandex.com>
2018-03-14 17:06:22 +0300

    Laravel Eticaret Kategori Seed Dosyası Yapıldı

--- eticaret/database/seeds/DatabaseSeeder.php
+++ eticaret/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+         $this->call(kategoriTableSeeder::class); #bu kod kategori seeeder dosyamızın çalışması için.
     }
 }
