450a41d711cdf5ed5ceafb84f111a86d9e2bdb0e
egva <fearfelix@yandex.com>
2018-03-15 00:32:17 +0300

    Laravel Eticaret Kategorideki Ürünlerin Listelenmesi

--- eticaret/resources/views/kategori.blade.php
+++ eticaret/resources/views/kategori.blade.php
@@ -50,30 +50,22 @@
                     <a href="#" class="btn btn-default">Yeni Ürünler</a>
                     <hr>
                     <div class="row">
+                        @if(count($urunler)=='0')
+                            <div class="col-md-12">Bu Kategoride Ürün Bulunmamaktadır.</div>
+                        @endif
+
+                        @foreach($urunler as $urun)
+
                         <div class="col-md-3 product">
-                            <a href="#"><img src="http://lorempixel.com/400/400/food/1"></a>
-                            <p><a href="#">Ürün adı</a></p>
-                            <p class="price">129 ₺</p>
+                            <a href="{{ Route('urun' , $urun->slug)}}"><img src="http://via.placeholder.com/400x400"></a>
+                            <p><a href="{{ Route('urun' , $urun->slug)}}">{{ $urun->urun_adi }}</a></p>
+                            <p class="price">{{ $urun->fiyati }} ₺</p>
                             <p><a href="#" class="btn btn-theme">Sepete Ekle</a></p>
                         </div>
-                        <div class="col-md-3 product">
-                            <a href="#"><img src="http://lorempixel.com/400/400/food/2"></a>
-                            <p><a href="#">Ürün adı</a></p>
-                            <p class="price">129 ₺</p>
-                            <p><a href="#" class="btn btn-theme">Sepete Ekle</a></p>
-                        </div>
-                        <div class="col-md-3 product">
-                            <a href="#"><img src="http://lorempixel.com/400/400/food/3"></a>
-                            <p><a href="#">Ürün adı</a></p>
-                            <p class="price">129 ₺</p>
-                            <p><a href="#" class="btn btn-theme">Sepete Ekle</a></p>
-                        </div>
-                        <div class="col-md-3 product">
-                            <a href="#"><img src="http://lorempixel.com/400/400/food/4"></a>
-                            <p><a href="#">Ürün adı</a></p>
-                            <p class="price">129 ₺</p>
-                            <p><a href="#" class="btn btn-theme">Sepete Ekle</a></p>
-                        </div>
+
+                        @endforeach
+
+
                     </div>
                 </div>
             </div>
