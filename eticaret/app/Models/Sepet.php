538e6925f132801026679b29c03ae1acf999499d
egva <fearfelix@yandex.com>
2018-03-20 15:29:59 +0300

    Laravel Eticaret Birden Fazla Sepet Yapısı

--- eticaret/app/Models/Sepet.php
+++ eticaret/app/Models/Sepet.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Facades\DB;
 class Sepet extends Model
 {
     protected $table= 'sepet';
@@ -13,6 +13,24 @@ class Sepet extends Model
     public function siparis() {
 
        return $this->hasOne('App\Models\Siparis');
+
+    }
+
+    public static function aktif_sepet_id() {
+
+        $aktif_sepet=DB::table('sepet as s')
+            ->leftJoin('siparis as si' , 'si.sepet_id' , '=','s.id')
+            ->where('s.kullanici_id', auth()->id())
+            ->whereRaw('si.id is null')
+            ->orderByDesc('s.created_at')
+            ->select('s.id')
+            ->first();
+
+            if (!is_null($aktif_sepet)) {
+                return $aktif_sepet->id;
+
+            }
+
 
     }
 
