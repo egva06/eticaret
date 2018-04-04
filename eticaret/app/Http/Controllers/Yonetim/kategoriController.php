a77e75d3e7edc4de80aa0ee3a873a16544885196
egva <fearfelix@yandex.com>
2018-04-04 15:44:33 +0300

    Laravel Eticaret Kategori Ekleme ve Düzenleme

--- eticaret/app/Http/Controllers/Yonetim/kategoriController.php
+++ eticaret/app/Http/Controllers/Yonetim/kategoriController.php
@@ -30,52 +30,37 @@ class kategoriController extends Controller
 
     public function form($id = 0) {
 
-        $entry = new Kullanici;
+        $entry = new Kategori;
         if ($id>0) {
-            $entry=Kullanici::find($id);
+            $entry=Kategori::find($id);
 
         }
-        return view('yonetim.kullanici.form' , compact('entry'));
+
+        $kategoriler = Kategori::all();
+
+        return view('yonetim.kategori.form' , compact('entry' , 'kategoriler'));
     }
 
     public function kaydet($id = 0) {
 
         $this->validate(request(),[
-            'adsoyad' => 'required',
-            'email' => 'required|email'
+            'kategori_adi' => 'required'
+
         ]);
 
-        $data= request()->only('adsoyad', 'email');
-        if (request()->filled('sifre')) {
-
-            $data['sifre'] = Hash::make(request('sifre'));
-        }
-        $data['aktif_mi']=request()->has('aktif_mi')&& request('aktif_mi')==1 ? 1:0;
-        $data['yonetici_mi']=request()->has('yonetici_mi') && request('yonetici_mi')==1 ? 1 : 0;
-
+        $data= request()->only('kategori_adi', 'slug' , 'ust_id');
 
         if ($id>0) {
 
-            $entry = Kullanici::where('id' , $id)->firstOrFail();
+            $entry = Kategori::where('id' , $id)->firstOrFail();
             $entry->update($data);
         } else {
 
-            $entry=Kullanici::create($data);
+            $entry=Kategori::create($data);
         }
 
-        KullaniciDetay::updateOrCreate(
-            ['kullanici_id' => $entry->id],
-            [
-                'adres'=> request('adres'),
-                'telefon' => request('telefon'),
-                'ceptelefonu' => request('ceptelefonu')
-            ]
-        );
-
-
-
         return redirect()
-            ->route('yonetim.kullanici.duzenle' , $entry->id)
+            ->route('yonetim.kategori.duzenle' , $entry->id)
             ->with('mesaj' , ($id>0 ? 'Güncellendi' : 'Kaydedildi'))
             ->with('mesaj_tur' , 'success');
     }
--- eticaret/resources/views/yonetim/kategori/form.blade.php
+++ eticaret/resources/views/yonetim/kategori/form.blade.php
@@ -5,7 +5,7 @@
 
 
 
-<form action="{{ route('yonetim.kullanici.kaydet' , @$entry->id) }}" method="post">
+<form action="{{ route('yonetim.kategori.kaydet' , @$entry->id) }}" method="post">
     {{ csrf_field() }}
 
     <div class="pull-right">
@@ -15,7 +15,7 @@
     </div>
 
     <h1 class="sub-header">
-        Kullanıcı {{ @$entry->id>0 ? "Düzenle" : "Ekle" }}
+        Kategori {{ @$entry->id>0 ? "Düzenle" : "Ekle" }}
     </h1>
 
     @include('layouts.partials.errors')
@@ -23,64 +23,35 @@
     <div class="row">
         <div class="col-md-6">
             <div class="form-group">
-                <label for="adsoyad">Ad Soyad</label>
-                <input type="text" class="form-control" id="adsoyad" placeholder="Ad Soyad" name="adsoyad" value="{{ old('adsoyad' , $entry->adsoyad) }}">
+                <label for="ust_id">Üst Kategori</label>
+                <select name="ust_id" id="ust_id" class="form-control">
+                    <option value="">Ana Kategori</option>
+                    @foreach($kategoriler as $kategori)
+                    <option value="{{ $kategori->id }}"> {{ $kategori->kategori_adi }}</option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+
+
+
+        <div class="col-md-6">
+            <div class="form-group">
+                <label for="kategori_adi">Kategori Adı</label>
+                <input type="text" class="form-control" id="kategori_adi" placeholder="Kategori Adı" name="kategori_adi" value="{{ old('kategori_adi' , $entry->kategori_adi) }}">
             </div>
         </div>
         <div class="col-md-6">
             <div class="form-group">
-                <label for="email">E-Mail</label>
-                <input type="email" class="form-control" id="email" placeholder="E Mail" name="email" value="{{  old('email' , $entry->email) }}">
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="form-group">
-                <label for="sifre">Şifre</label>
-                <input type="password" class="form-control" id="sifre" name="sifre"  placeholder="Şifre">
+                <label for="slug">Slug</label>
+                <input type="text" class="form-control" id="slug" placeholder="Slug" name="slug" value="{{  old('slug' , $entry->slug) }}">
             </div>
         </div>
 
-    </div>
-    <div class="row">
-        <div class="col-md-12">
-            <div class="form-group">
-                <label for="adres">Adres</label>
-                <input type="text" class="form-control" id="adres" placeholder="Adres" name="adres" value="{{old('adres' ,$entry->detay->adres) }}">
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12">
-            <div class="form-group">
-                <label for="telefon">Telefon</label>
-                <input type="text" class="form-control" id="telefon" placeholder="Telefon" name="telefon" value="{{ old('telefon' ,$entry->detay->telefon) }}">
-            </div>
-        </div>
+
     </div>
 
-    <div class="row">
-        <div class="col-md-12">
-            <div class="form-group">
-                <label for="ceptelefonu">Cep Telefonu</label>
-                <input type="text" class="form-control" id="ceptelefonu" placeholder="Cep Telefon" name="ceptelefonu" value="{{old('ceptelefonu' , $entry->detay->ceptelefonu) }}">
-            </div>
-        </div>
-    </div>
-    <div class="checkbox">
-        <label>
-            {{-- Hidden input kullanma sebebimiz old ile eski girilen değeri alabilmek için checkboxdan--}}
 
-            <input type="hidden" name="aktif_mi" value="0">
-            <input type="checkbox" name="aktif_mi" value="1" {{old('aktif_mi' ,$entry->aktif_mi) ? 'checked' : '' }}> Aktif Mi
-        </label>
-    </div>
-
-    <div class="checkbox">
-        <label>
-            <input type="hidden" name="yonetici_mi" value="0">
-            <input type="checkbox" name="yonetici_mi" value="1" {{old('yonetici_mi', $entry->yonetici_mi) ? 'checked' : '' }} > Yönetici Mi
-        </label>
-    </div>
 
 </form>
 
