b8012b33977773c986dfaf411e09dcfa75999cc1
egva <fearfelix@yandex.com>
2018-04-12 13:58:13 +0300

    Laravel Eticaret Sipariş Detay Bilgilerinin Güncellenmesi

--- eticaret/app/Http/Controllers/Yonetim/siparisController.php
+++ eticaret/app/Http/Controllers/Yonetim/siparisController.php
@@ -50,67 +50,33 @@ class siparisController extends Controller
     public function kaydet($id = 0)
     {
 
-        $data = request()->only('urun_adi', 'slug', 'aciklama', 'fiyati');
-        if (!request()->filled('slug')) {
 
-            $data['slug'] = str_slug(request('urun_adi'));
-            request()->merge(['slug' => $data['slug']]);
 
-        }
 
         $this->validate(request(), [
-            'urun_adi' => 'required',
-            'fiyati' => 'required',
-            'slug' => (request('original_slug') != request('slug') ? 'unique:urun,slug' : '')
+            'adsoyad' => 'required',
+            'adres' => 'required',
+            'telefon' => 'required',
+            'ceptelefonu' => 'required',
+            'durum' => 'required'
 
         ]);
 
-        $data_detay = request()->only('goster_slider', 'goster_gunun_firsati', 'goster_one_cikan', 'goster_cok_satan', 'goster_indirimli');
+        $data = request()->only('adsoyad', 'adres', 'telefon', 'ceptelefonu' , 'durum');
 
-        $kategoriler = request('kategoriler');
 
         if ($id > 0) {
 
-            $entry = Urun::where('id', $id)->firstOrFail();
+            $entry = Siparis::where('id', $id)->firstOrFail();
             $entry->update($data);
 
-            $entry->detay()->update($data_detay);
-            $entry->kategoriler()->sync($kategoriler);
-
-        } else {
-
-            $entry = Urun::create($data);
-            $entry->detay()->create($data_detay);
-            $entry->kategoriler()->attach($kategoriler);
 
         }
-
-        if (request()->hasFile('urun_resmi') ) {
-
-            $this->validate(request(),[
-             'urun_resmi' => 'image|mimes:jpg,png,jpeg,gif|max:5000'
-
-            ]);
-            $urun_resmi = request()->file('urun_resmi');
-
-            $dosyaadi = $entry->id . "-" . time() . "." . $urun_resmi->extension();
-
-            if ($urun_resmi->isValid()) {
-
-                $urun_resmi->move('uploads/urunler' , $dosyaadi);
-                UrunDetay::updateOrCreate(
-                    ['urun_id' => $entry->id],
-                    ['urun_resmi' => $dosyaadi]
-                );
-            }
-
-        }
-
 
 
         return redirect()
-            ->route('yonetim.urun.duzenle', $entry->id)
-            ->with('mesaj', ($id > 0 ? 'Güncellendi' : 'Kaydedildi'))
+            ->route('yonetim.siparis.duzenle', $entry->id)
+            ->with('mesaj', ('Güncellendi'))
             ->with('mesaj_tur', 'success');
     }
 
